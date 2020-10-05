1d1d40d3187f02179f78d46ecab2314ada5cb7a6
nena03 <69650515+nena03@users.noreply.github.com>
2020-10-05 22:48:23 +0200

    First commit

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.IO;
+using Proslava;
+
+namespace Proslava
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Proslava[] pr = new Proslava[100];
+        int n = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Proslava[] pr ={
+                              (Proslava)(new Osoba("Milos","Petrovic", 45, "06003564889")),
+                              (Proslava)(new Osoba("Ana","Petrovic", 35, "0600356475")),
+                              (Proslava)(new Osoba("Goran","Katic", 47, "061223456")),
+                              (Proslava)(new Osoba("Minja","Kujundzic", 25, "060226544")),
+                              (Proslava)(new Osoba("Olja","Jovanovic", 34, "0665547822"))};
+
+            for (int i = 0; i < pr.Length; i++)
+            {
+                if (pr[i] is Osoba)
+                    listBox1.Items.Add("Osoba: " + pr[i].Info());
+
+            }
+
+
+            for (int i = 0; i < pr.Length; i++)
+            {
+                for (int j = i + 1; j < pr.Length; j++)
+                {
+                    if (pr[j].StarijeOd(pr[i]))
+                    {
+                        Proslava pom = pr[i];
+                        pr[i] = pr[j];
+                        pr[j] = pom;
+                    }
+                }
+            }
+
+            for (int i = 0; i < pr.Length; i++)
+            {
+                if (pr[i] is Osoba)
+                    listBox2.Items.Add("Osoba: " + pr[i].Info());
+
+            }
+
+        }
+    }
+}
---
+++ Proslava.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Proslava
+{
+
+    public abstract class Proslava
+    {
+
+        protected string ime, prezime;
+        protected int godiste;
+        protected string telefon;
+
+        public Proslava(string i, string p, int g, string t)
+        {
+            if (i.Equals(string.Empty))
+                throw new Exception("Nema ime!");
+            else if (p.Equals(string.Empty))
+                throw new Exception("Nema prezime!");
+            else if (t.Equals(string.Empty))
+                throw new Exception("Nema telefon!");
+
+            ime = i;
+            prezime = p;
+            telefon = t;
+            godiste = g;
+
+
+        }
+        public bool StarijeOd(Proslava pr)
+        {
+            return godiste < pr.godiste;
+        }
+        public string Prikaz()
+        {
+            return ime + " " + prezime + " " + godiste + " " + telefon;
+        }
+
+        public abstract string Info();
+
+    }
+    public class Osoba : Proslava
+    {
+        private int godine;
+        public Osoba(string i, string p, int g, string t)
+            : base(i, p, g, t)
+        {
+            if (g < 0 || g > 66)
+                throw new Exception("Neispravno unete godine!");
+            godine = g;
+        }
+        public override string Info()
+        {
+            string s = ime + " " + prezime;
+            s += ", tel: " + telefon;
+            s += ", god: " + godine;
+            return s;
+        }
+
+    }
+}
+
